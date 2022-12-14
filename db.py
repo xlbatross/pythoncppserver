6a58eb25cc56e0782f3806e4e077c94cdb6ec8f8
rkgl0507 <rkgl0507@naver.com>
2022-12-14 17:36:50 +0900

    회원가입 수정

--- db.py
+++ db.py
@@ -42,6 +42,11 @@ class DB:
     def signUp(self,name,num,pw,cate):
         conn = self.connect()
         cursor = conn.cursor() 
+        sql = f"""select num from lecture.user_info where num = "{num}";"""
+        cursor.execute(sql)
+        result = cursor.fetchone()
+        if result != None:
+            return False, "이미 존재하는 학번입니다"
         sql = f"""insert into lecture.user_info (name,num,pw,cate) value ("{name}","{num}", "{pw}","{cate}");"""
         cursor.execute(sql)
         conn.commit()
